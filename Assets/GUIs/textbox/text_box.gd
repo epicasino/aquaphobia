0f1c99cf0bc553f6e08d1f23245e7c27500892d3
Ray Badua <rayjonathanbadua@gmail.com>
2024-05-16 05:35:04 -0700

    textboxing stuff again

--- Assets/GUIs/textbox/text_box.gd
+++ Assets/GUIs/textbox/text_box.gd
@@ -1,5 +1,29 @@
 extends MarginContainer
 
-@onready var label = $MarginContainer/Label
+@onready var richText = $MarginContainer/RichTextLabel
+@onready var type_timer = $type_timer
+@onready var wait_timer = $wait_timer
 
-@export var max_width = 256
+signal message_completed
+
+func update_message(message: String):
+	richText.bbcode_text = _extract_tags(message)
+	richText.visible_characters = 0
+	for i in richText.text:
+		wait_timer.wait_time += 0.05
+	type_timer.start()
+	wait_timer.start()
+
+func _on_type_timer_timeout():
+	if richText.visible_characters < richText.text.length():
+		richText.visible_characters += 1
+	else:
+		type_timer.stop()
+
+func _extract_tags(source_string: String):
+	var _custom_regex = RegEx.new()
+	_custom_regex.compile("({(.*?)})")
+	return _custom_regex.sub(source_string, "", true)
+
+func _on_wait_timer_timeout():
+	message_completed.emit()
--- Assets/Interactables/screen.gd
+++ Assets/Interactables/screen.gd
@@ -1,1 +1,32 @@
 extends Area2D
+
+@onready var text_box = $text_box
+var playerInTerminal = false
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+
+func _process(delta):
+	if playerInTerminal && Input.is_action_pressed("interact"):
+		$turn_on_timer.start()
+
+func _on_body_entered(body):
+	if body.name == 'Player':
+		playerInTerminal = true
+
+func _on_body_exited(body):
+	if body.name == 'Player':
+		playerInTerminal = false
+
+func _on_text_box_message_completed():
+	print('hello')
+	$AnimatedSprite2D.play("turn_off")
+
+func _on_turn_on_timer_timeout():
+	$officer_hello.play()
+	$AnimatedSprite2D.play("talking")
+	text_box.update_message("[center]" + "[wave]Hi[/wave] I was generated for the dialogue system test for the godot game engine" + "[/center]")
+
+#func _on_animated_sprite_2d_animation_finished(animation):
+	#if animation == 'turn_off':
+		#$AnimatedSprite2D.play("off")
